aa57dbedb306f0bb4b881069ef51ecb66bc9f172
Yevhen <mazuren83@gmail.com>
2025-05-14 18:01:32 +0300

    homework 2 fix

--- tests/homework-2/demo-learnwebdriverio.spec.ts
+++ tests/homework-2/demo-learnwebdriverio.spec.ts
@@ -3,30 +3,30 @@ import { test, expect } from '@playwright/test';
 test.describe('tests set for auth page', ()=>{
   test('DL-T1 registeration', { tag: ["@regression", "@smoke"] }, async({page}) => {
     await page.goto('https://demo.learnwebdriverio.com/register');
-    await page.locator('//input[@placeholder="Username"]').fill('Zheka4');
-    await page.locator('//input[@placeholder="Email"]').pressSequentially('xixubrubruma-2272@yopmail.com', { delay: 100});
+    await page.locator('//input[@placeholder="Username"]').fill('Zheka5');
+    await page.locator('//input[@placeholder="Email"]').pressSequentially('fellolenitri-3810@yopmail.com', { delay: 100});
     await page.locator('//input[@placeholder="Password"]').fill('qwerty');
     await page.locator("//button[contains(text(),'Sign up')]").click();
-    await expect(page.getByRole('link', { name: 'zheka4' })).toContainText('zheka4');
+    await expect(page.getByRole('link', { name: 'Zheka5' })).toContainText('Zheka5');
   }) 
   test('DL-T2 login', { tag: ["@regression", "@smoke"]}, async({page}) => {
     await page.goto('https://demo.learnwebdriverio.com/login');
-    await page.locator('//input[@placeholder="Email"]').pressSequentially('xixubrubruma-2272@yopmail.com', { delay: 100});
+    await page.locator('//input[@placeholder="Email"]').pressSequentially('fellolenitri-3810@yopmail.com', { delay: 100});
     await page.locator('//input[@placeholder="Password"]').fill('qwerty');
     await page.locator("//button[contains(text(),'Sign in')]").click();
-    await expect(page.getByRole('link', { name: 'zheka4' })).toContainText('zheka4');
+    await expect(page.getByRole('link', { name: 'Zheka5' })).toContainText('Zheka5');
   })
   test('DL-T3 login with incorrect credentials', { tag: ["@regression", "@smoke"]}, async({page}) => {
     await page.goto('https://demo.learnwebdriverio.com/login');
-    await page.locator('//input[@placeholder="Email"]').pressSequentially('xixubrubruma-2272@yopmail.com', { delay: 100});
+    await page.locator('//input[@placeholder="Email"]').pressSequentially('fellolenitri-3810@yopmail.com', { delay: 100});
     await page.locator('//input[@placeholder="Password"]').fill('qwertys');
     await page.locator("//button[contains(text(),'Sign in')]").click();
     await expect(page.locator('//ul[@class="error-messages"]/li')).toContainText('email or password is invalid');
   })
   test('DL-T4 Register with existed user', { tag: ["@regression", "@smoke"]}, async({page}) => {
     await page.goto('https://demo.learnwebdriverio.com/register');
-    await page.locator('//input[@placeholder="Username"]').fill('Zheka4');
-    await page.locator('//input[@placeholder="Email"]').pressSequentially('xixubrubruma-2272@yopmail.com', { delay: 100});
+    await page.locator('//input[@placeholder="Username"]').fill('Zheka5');
+    await page.locator('//input[@placeholder="Email"]').pressSequentially('fellolenitri-3810@yopmail.com', { delay: 100});
     await page.locator('//input[@placeholder="Password"]').fill('qwerty');
     await page.locator("//button[contains(text(),'Sign up')]").click();
     await expect(page.locator('//ul[@class="error-messages"]/li[1]')).toContainText('username is already taken.');
